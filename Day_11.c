fb4892d17f01b90ae3dcecd5c4f8b64f04b1a5dd
Piyush Agrawal <agrawallpiyush10@gmail.com>
2026-02-11 10:26:01 +0530

    Add files via upload

---
+++ Day_11.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() 
+{
+    int rows, cols;
+
+    scanf("%d %d", &rows, &cols);
+
+    int first[rows][cols];
+    int second[rows][cols];
+    int result[rows][cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            scanf("%d", &first[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            scanf("%d", &second[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            result[i][j] = first[i][j] + second[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
