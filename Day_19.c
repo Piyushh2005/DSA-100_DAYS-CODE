7807747a8c7d5f107d1e38663e8724daa428de96
Piyush Agrawal <agrawallpiyush10@gmail.com>
2026-02-19 16:46:32 +0530

    Add files via upload

---
+++ Day_19.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int cmp(const void *a, const void *b)
+{
+    return (*(int*)a - *(int*)b);
+}
+
+void closestToZero(int arr[], int n)
+{
+    qsort(arr, n, sizeof(int), cmp);
+
+    int left = 0;
+    int right = n - 1;
+
+    int min_sum = INT_MAX;
+    int x = 0, y = 0;
+
+    while (left < right)
+    {
+        int sum = arr[left] + arr[right];
+
+        if (abs(sum) < abs(min_sum))
+        {
+            min_sum = sum;
+            x = arr[left];
+            y = arr[right];
+        }
+
+        if (sum < 0)
+            left++;
+        else
+            right--;
+    }
+
+    printf("%d %d", x, y);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    closestToZero(arr, n);
+
+    return 0;
+}
